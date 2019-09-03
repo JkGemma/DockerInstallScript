12c0c6cfb5818eda4ec220517bf8b4d58738f0b6
JkGemma <quentin.flayac@epitech.eu>
2019-09-03 16:24:07 +0200

    Update docker bionic stable

--- Docker_bionicStable.sh
+++ Docker_bionicStable.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 sudo apt-get -y install apt-transport-https \
-ca-certificates \
-curl \
-gnupg \
-software-properties-common \
+    ca-certificates \
+    curl \
+    gnupg \
+    software-properties-common \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -14,7 +14,7 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" |
 sudo apt-get update
 
 sudo apt-get -y install docker-ce \
-docker-compose
+    docker-compose
 
 sudo usermod -aG docker $USER
 
